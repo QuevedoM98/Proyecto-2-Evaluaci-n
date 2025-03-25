f13756d2b7cc1e03c87b26d868c6977dbafd373f
2024gm2dylangmail.com <2024gm2dylangmail.com>
2025-03-25 19:35:18 +0100

    He creado la clase Usuario

---
+++ src/main/java/model/Colaborador.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Colaborador extends Usuario {
+    private int puntos;
+
+    public Colaborador(String nombre, String usuario, String contrasena, String correoElectronico) {
+        super(nombre, usuario, contrasena, correoElectronico);
+        this.puntos = 0;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void incrementarPuntos(int puntos) {
+        this.puntos += puntos;
+    }
+}

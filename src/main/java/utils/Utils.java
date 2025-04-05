07dbc3daddd318e501418715ce96be89555d8ca3
2024gm2dylangmail.com <2024gm2dylangmail.com>
2025-04-05 23:11:23 +0200

    Depuración en la clase Utils

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -5,33 +5,7 @@ import java.util.Scanner;
 public class Utils {
     static Scanner scanner = new Scanner(System.in);
 
-    /**
-     * Valida un ISBN en formato estándar (ISBN-13).
-     * El formato debe ser: 978-XX-XXXXXXX-X
-     * @param isbn ISBN a validar
-     * @return true si el ISBN es válido, false si no lo es
-     */
-    public static boolean validarISBN(String isbn){
-        String isbnRegex = "^97[8-9]-[0-9]{2}-[0-9]{7}-[0-9]$";
-        return isbn.matches(isbnRegex);
-    }
 
-    /**
-     * Valida un ISBN en formato de revista.
-     * El formato debe ser: XXXX-XXXX
-     * @param isbn ISBN a validar
-     * @return true si el ISBN es válido, false si no lo es
-     */
-    public static boolean validarISBNrevista (String isbn){
-        String isbnRegex = "^[0-9]{4}-[0-9]{4}$";
-        return isbn.matches(isbnRegex);
-    }
-
-    /**
-     * Lee un número entero desde la entrada estándar, asegurándose de que es un valor válido.
-     * @param mensaje Mensaje que se muestra al usuario antes de ingresar el número
-     * @return El número entero ingresado por el usuario
-     */
     public static int leeEntero(String mensaje) {
         int numero = 0;
         boolean valido = false;
